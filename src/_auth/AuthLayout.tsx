e88de856104a723bb4869b7808e0da6ffa244730
kalakhhata <ompatel2501@gmail.com>
2024-11-14 17:32:17 -0500

    Today I learnd about the Appwrite Database and I set it up so I can save my account info in that,I have also learned about zod library to validate the input

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,12 +1,21 @@
-import { Section } from 'lucide-react';
-import React from 'react'
 import {Outlet , Navigate} from "react-router-dom"
 
 function AuthLayout() {
-  const Authenticated =true;
+  const Authenticated =false;
   return (
     <>
-      {Authenticated ? (<Navigate to="/" />) : (<><section><Outlet /></section></>)}
+      {Authenticated ? (<Navigate to="/" />) : 
+      (
+      <>
+      <section className="flex flex-1 justify-center items-center flex-col py-10">
+        <Outlet />
+      </section>
+
+      <img src="/assets/images/side-img.svg"
+           alt="logo"
+           className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat" />
+        </>
+      )}
     </>
   )
 }
